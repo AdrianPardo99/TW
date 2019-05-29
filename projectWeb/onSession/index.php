889a36bd1b5b97b4aff702eef3227af71cad6918
Adrian González Pardo <gozapaadr@gmail.com>
2019-05-28 22:12:04 -0500

    Navbar Session, responsive nav in mobile
    SQL

--- projectWeb/onSession/index.php
+++ projectWeb/onSession/index.php
@@ -1,1 +1,12 @@
-<?php header('Location: formulario1.php');?>
+<?php
+include '../php/headerSession.php';
+$var = "onsession";
+include '../php/onsesionfilenav.php';
+session_start();
+if($_SESSION["ok"] == 1){?>
+
+<?php
+include '../php/footerSession.php';
+}else{
+  header("location:../php/login.php");
+}?>
